07fe66b51160f3ab4fa94e85ff2fbf6305fc5b9f
Anurag Kumar <anuragba01@gmail.com>
2026-05-30 10:14:00 +0530

    Initial commit: Set up repository structure and .gitignore

---
+++ tailwind-config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Plus Jakarta Sans', 'sans-serif'],
+            },
+            colors: {
+                brand: {
+                    50: '#f5f3ff', 100: '#ede9fe', 200: '#ddd6fe',
+                    500: '#6366f1', 600: '#4f46e5', 700: '#4338ca',
+                },
+                darkbg: '#0B0F19',
+                darkcard: '#111827',
+                lightbg: '#FAF6EE', // Premium Cream Background
+                lightcard: '#FFFFFF',
+            }
+        }
+    }
+}
